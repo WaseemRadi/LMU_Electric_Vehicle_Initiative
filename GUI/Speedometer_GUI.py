366abed3c13296480ac5ef5ba201b4a1ffa5104f
Waseem Radi <WaseemRadi@users.noreply.github.com>
2017-10-20 19:34:11 -0700

    Update Speedometer_GUI.py

--- GUI/Speedometer_GUI.py
+++ GUI/Speedometer_GUI.py
@@ -10,15 +10,14 @@ class speedometer(object):
     self.display = self.canvas.create_oval(50, 50, 450, 450)
     self.speed_hand = self.canvas.create_line(250, 250, 250, 450)
     self.root.after(1 , self.animation)
-    self.root.mainloop() 
-  
+    self.root.mainloop()  
   def animation(self):
-    while 1 == 1:
-      for angle in range(0, 10):
-        time.sleep(.0025)
-        x = 200 * math.sin(angle)
-        y = 200 * math.cos(angle) 
-        self.canvas.coords(self.speed_hand, 250, 250, int(x), int(y))
-        self.canvas.update()
+    for angle in range(0, 400):
+      time.sleep(.05)
+      x = 200 * math.sin(angle) + 250
+      y = (200 * math.cos(angle)) + 250
+      print(x, y)
+      self.canvas.coords(self.speed_hand, 250, 250, int(x), int(y))
+      self.canvas.update()
         
 speedometer()
