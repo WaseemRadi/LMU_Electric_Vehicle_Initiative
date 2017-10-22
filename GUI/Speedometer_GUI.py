36ee34b8d585c6e093ce543703f4b218417222dc
Waseem Radi <WaseemRadi@users.noreply.github.com>
2017-10-21 19:29:48 -0700

    Update Speedometer_GUI.py

--- GUI/Speedometer_GUI.py
+++ GUI/Speedometer_GUI.py
@@ -5,12 +5,27 @@ import time
 class speedometer(object):
   def __init__(self):
     self.root = Tk()
-    self.canvas = Canvas(self.root, width = 500, height = 500)
+    self.canvas = Canvas(self.root, width = 500, height = 500, bg = 'black')
     self.canvas.pack() 
-    self.display = self.canvas.create_oval(50, 50, 450, 450)
+    self.display = self.canvas.create_oval(25, 25, 475, 475, fill = '#C0C0C0')
+    self.display2 = self.canvas.create_oval(50, 50, 450, 450, fill = 'white')
     self.speed_hand = self.canvas.create_line(250, 250, 250, 450)
+    for i in range(2,15):
+      ang = i * math.pi / 8 
+      x = 225 * math.sin(ang) + 250
+      y = 225 * math.cos(ang) + 250 
+      x1 = 200 * math.sin(ang) + 250 
+      y1 = 200 * math.cos(ang) + 250
+      self.canvas.create_line(int(x), int(y), int(x1), int(y1))   
+    for i in range(3,30): 
+      ang = i * math.pi / 16 
+      x = 225 * math.sin(ang) + 250
+      y = 225 * math.cos(ang) + 250 
+      x1 = 212 * math.sin(ang) + 250 
+      y1 = 212 * math.cos(ang) + 250
+      self.canvas.create_line(int(x), int(y), int(x1), int(y1))      
     self.root.after(1 , self.animation)
-    self.root.mainloop()  
+    self.root.mainloop()   
   def animation(self):
     for angle in range(400, 0, -1):
       time.sleep(.025)
