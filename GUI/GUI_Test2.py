c1d4ca0b6df871957f2feba798ef418aa344fcab
Waseem Radi <WaseemRadi@users.noreply.github.com>
2017-10-24 10:27:25 -0700

    Update GUI_Test2.py

--- GUI/GUI_Test2.py
+++ GUI/GUI_Test2.py
@@ -97,7 +97,8 @@ class speedometer(object):
           x = 200 * math.sin(5.495-.0785*velocity) + 250
           y = (200 * math.cos(5.495-.0785*velocity)) + 250
           self.canvas.coords(self.speed_hand, 250, 250, int(x), int(y))
-          self.canvas.create_text(250, 300, text = str(velocity), font = 'helvetica 20 bold')
+          t1 = self.canvas.create_text(250, 300, font = 'helvetica 20 bold')
+          self.canvas.itemconfigure(t1, text = str(velocity))
           self.canvas.update()
               
     except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
